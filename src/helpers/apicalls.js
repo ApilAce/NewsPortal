487e50d0c5fb524b71d242a8070a2ef1b77a9583
Praful360 <53175654+Praful360@users.noreply.github.com>
2023-05-14 19:09:54 +0545

    aaded axios syntax

--- src/helpers/apicalls.js
+++ src/helpers/apicalls.js
@@ -6,6 +6,11 @@ const GET_TEST_URL = (params) => {
     return GET(process.env.NEXT_PUBLIC_TEST_API);
 }
 
+const GET_CATEGORIES = () => {
+    return GET(process.env.NEXT_PUBLIC_CATEGORY_API);
+}
+
 export {
-    GET_TEST_URL
+    GET_TEST_URL,
+    GET_CATEGORIES
 }
--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,21 +1,34 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import Layout from '@/components/admin/layout'
-import { GET_TEST_URL } from '@/helpers/apicalls'
-import { useRouter } from 'next/router';
+import { GET_CATEGORIES, GET_TEST_URL } from '@/helpers/apicalls'
 
-export default function Dashboard() {
-    useEffect(() => {
-        test();
-    }, []);
+export const getServerSideProps = async(context: any) => {
+  const categories = await GET_CATEGORIES();
+  return {
+    props: {
+      data: categories
+    } 
+  }
+}
 
-    const test = async() => {
-        const data = await GET_TEST_URL();
-    }
-    
+export default function Dashboard({data}: any) {
+  const [categories, setCategories] = useState<any>([]);
+  useEffect(() => {
+    setCategories(data);
+  }, []);
+
   return (
     <>
       <Layout title="Admin | Dashboard">
         <h2>This is body of Admin</h2>
+        {categories.length > 2 ? categories.map((category: any, index: number) => {
+          return(
+            <>
+              <h1>{category?.name}</h1>
+            </>
+          );
+        
+        }) : <h1>NotFOund</h1>}
       </Layout>
     </>
   )

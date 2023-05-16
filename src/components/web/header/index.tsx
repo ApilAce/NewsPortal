75f56750512f6b91f5b93e02d741bf6ef6d25c16
Luffy <birajr0@gmail.com>
2023-05-16 17:31:03 +0545

    Adds sub header in header component

--- src/components/web/header/index.tsx
+++ src/components/web/header/index.tsx
@@ -1,11 +1,11 @@
+import clsx from "clsx";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import styles from "./header.module.css";
+import { useEffect, useState } from "react";
+import { IoIosCalendar } from "react-icons/io";
 import en from "../../../locales/en/header.json";
 import np from "../../../locales/np/header.json";
-import clsx from "clsx";
-import { useState } from "react";
-import { IoIosCalendar } from "react-icons/io";
+import styles from "./header.module.css";
 
 const languageData = [
   {
@@ -24,6 +24,16 @@ const languageData = [
   },
 ];
 
+const subHeaderData = {
+  leftData: [
+    { title: "trending" },
+    { title: "team" },
+    { title: "advertisement" },
+    { title: "thoughts" },
+  ],
+  rightData: [{ title: "membership" }, { title: "trending" }],
+};
+
 const Header = () => {
   const router = useRouter();
   const { locale } = router;
@@ -31,50 +41,93 @@ const Header = () => {
 
   const handleLanguageChange = (language: string) => {
     setLanguage(language);
+    localStorage.setItem("language", language); // Store in localStorage
     router.push(router.pathname, router.asPath, { locale: language });
   };
 
-  const t = locale === "np" ? np : en;
+  const t: any = locale === "np" ? np : en;
+
+  useEffect(() => {
+    // Retrieve value from localStorage
+    const storedLanguage = localStorage.getItem("language");
+
+    // Set the stored value as initial state if it exists
+    if (storedLanguage) {
+      setLanguage(storedLanguage);
+    }
+  }, []);
 
   return (
-    <div className={styles.container}>
-      <div className="container">
-        <header className={styles.header}>
-          <div className={styles.newsContainer}>
-            <div className={styles.dateContainer}>
-              <IoIosCalendar className={styles.dateIcon} />
-              <div className={styles.date}>{t.date}</div>
+    <>
+      {/* <Head>
+        {locales.map((locale) => (
+          <link
+            rel="alternate"
+            hrefLang={locale}
+            href={`https://yourwebsite.com/${locale}${router.asPath}`}
+            key={locale}
+          />
+        ))}
+      </Head> */}
+      <div className={styles.container}>
+        <div className="container">
+          <header className={styles.header}>
+            <div className={styles.newsContainer}>
+              <div className={styles.dateContainer}>
+                <IoIosCalendar className={styles.dateIcon} />
+                <div className={styles.date}>{t.date}</div>
+              </div>
+              <div className={styles.category}>{t.trending}</div>
+              <div className={styles.news}>{t.news}</div>
             </div>
-            <div className={styles.category}>{t.trending}</div>
-            <div className={styles.news}>{t.news}</div>
-          </div>
-          <div className={styles.languageContainer}>
-            <div className={styles.language}>{t.language}</div>
-            <div className={styles.iconContainer}>
-              {languageData.map((item) => (
-                <div
-                  className={clsx(
-                    styles.imageContainer,
-                    language === item.lng ? styles.active : ""
-                  )}
-                  onClick={() => handleLanguageChange(item.lng)}
-                  key={item.alt}
-                >
-                  <Image
-                    src={item.src}
-                    alt={item.alt}
-                    className={styles.image}
-                    width={item.width}
-                    height={item.height}
-                  ></Image>
+            <div className={styles.languageContainer}>
+              <div className={styles.language}>{t.language}</div>
+
+              <div className={styles.iconContainer}>
+                {languageData.map((item) => (
+                  <div
+                    className={clsx(
+                      styles.imageContainer,
+                      language === item.lng ? styles.active : ""
+                    )}
+                    onClick={() => handleLanguageChange(item.lng)}
+                    key={item.alt}
+                  >
+                    <Image
+                      src={item.src}
+                      alt={item.alt}
+                      className={styles.image}
+                      width={item.width}
+                      height={item.height}
+                    ></Image>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </header>
+        </div>
+      </div>
+      <div className={styles.subWrapper}>
+        <div className={"container"}>
+          <header className={styles.subHeader}>
+            <div className={styles.subHeaderList}>
+              {subHeaderData.leftData.map((headerItem) => (
+                <div className={styles.subHeaderTitle} key={headerItem.title}>
+                  {t[headerItem.title]}
                 </div>
               ))}
             </div>
-          </div>
-        </header>
-        <header className={styles.subHeader}></header>
+            <div className={styles.subHeaderList}>
+              {subHeaderData.rightData.map((headerItem) => (
+                <div className={styles.subHeaderTitle} key={headerItem.title}>
+                  {t[headerItem.title]}
+                </div>
+              ))}
+            </div>
+          </header>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,12 +3,17 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import en from "../../locales/en/common.json";
 import np from "../../locales/np/common.json";
+import Head from "next/head";
 
 const Homepage = () => {
   const { locale } = useRouter();
   const t = locale === "np" ? np : en;
   return (
     <WebsiteLayout>
+      <Head>
+        <title>News Portal Homepage</title>
+        <meta name="description" content="News Portal Homepage" />
+      </Head>
       <div>{t.home}</div>
       <Link href={"/about/"}>{t.about}</Link>
     </WebsiteLayout>

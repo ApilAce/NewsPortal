8b5a67bc344b2b1dbe55ad58a7763077210ed958
Luffy <birajr0@gmail.com>
2023-05-22 10:55:47 +0545

    Removes ant design

--- src/components/admin/layout.tsx
+++ src/components/admin/layout.tsx
@@ -1,61 +1,7 @@
-import { ConfigProvider, Layout, theme } from "antd";
-import Sidebar from "./sidebar/sidebar";
-import Navbar from "./navbar/navbar";
-import React from "react";
-
-const { Header, Content, Sider } = Layout;
+import { Layout } from "antd";
 
 const AdminLayout = ({ children, title }: any) => {
-  const {
-    token: { colorBgContainer },
-  } = theme.useToken();
-  return (
-    <>
-      <ConfigProvider
-        theme={{
-          token: {
-            fontFamily: "Poppins",
-          },
-        }}
-      >
-        <Layout className="main-layout">
-          <Sider
-            // breakpoint="md"
-            // collapsedWidth="0"
-            width={250}
-            style={{ height: "100vh", overflow: "auto" }}
-            theme="light"
-            trigger={null}
-            collapsible
-            className="ant-sider"
-          >
-            <div className="demo-logo-vertical" />
-            <Sidebar />
-          </Sider>
-          <Layout className="main-ant-wrapper">
-            <Header
-              style={{
-                backgroundColor: "white",
-                padding: "0",
-                height: "fit-content",
-              }}
-            >
-              <Navbar />
-            </Header>
-            <Content
-              style={{
-                margin: "1.125em 1em",
-                padding: "1em",
-                minHeight: 280,
-              }}
-            >
-              {children}
-            </Content>
-          </Layout>
-        </Layout>
-      </ConfigProvider>
-    </>
-  );
+  return <>{children}</>;
 };
 
 export default AdminLayout;
--- src/components/admin/sidebar/sidebar.tsx
+++ src/components/admin/sidebar/sidebar.tsx
@@ -1,53 +1,5 @@
-import {
-  AppstoreOutlined,
-  BarChartOutlined,
-  CloudOutlined,
-  ShopOutlined,
-  TeamOutlined,
-  UploadOutlined,
-  UserOutlined,
-  VideoCameraOutlined,
-} from "@ant-design/icons";
-import { Menu, MenuProps } from "antd";
-import React from "react";
-import styles from "./sidebar.module.css";
-import { useRouter } from "next/router";
-
 const Sidebar = () => {
-  
-const router = useRouter();
-
-const items: MenuProps["items"] = [
-  UserOutlined,
-  VideoCameraOutlined,
-  UploadOutlined,
-  BarChartOutlined,
-  ShopOutlined,
-  TeamOutlined,
-  CloudOutlined,
-  AppstoreOutlined,
-].map((icon, index) => ({
-  key: String(index + 1),
-  icon: React.createElement(icon),
-  label: `nav ${index + 1}`
-}));
-
-  return (
-    <div>
-      <h2 className={styles.title}>News Portal</h2>
-      <Menu
-        theme="light"
-        mode="inline"
-        defaultSelectedKeys={["1"]}
-        items={items}
-        onClick={({ item, key, keyPath, domEvent }) => {
-          key == '1' ? router.push('/admin') : ''
-          key == '2' ? router.push('/admin/category') : ''
-        }}
-        className={styles.menu}
-      />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Sidebar;
